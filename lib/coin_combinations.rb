da9406f40b626bc466634cc8d4c6ef038a5fa9a8
abdul rahimi & chris underwood <student@epicodus.com>
2015-08-06 16:04:28 -0700

    working ruby coin_combinations puzzle

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,7 +1,35 @@
-class Fixnum
+class Float
   define_method(:coin_combinations) do
-    total_change = self.to_f
+    quarters = 0
+    dimes = 0
+    nickels = 0
+    pennies = 0
+    total_change = self
 
+    until total_change.<(0.25)
+      total_change = total_change.-(0.25).round(2)
+      quarters = quarters.+(1)
+    end
+
+    until total_change.<(0.10)
+      total_change = total_change.-(0.10).round(2)
+      dimes = dimes.+(1)
+    end
+
+    until total_change.<(0.05)
+      total_change = total_change.-(0.05).round(2)
+      nickels = nickels.+(1)
+    end
+
+    until total_change.<=(0.00)
+      total_change = total_change.-(0.01).round(2)
+      pennies = pennies.+(1)
+    end
+
+    puts "You have #{quarters} quarters"
+    puts "You have #{dimes} dimes"
+    puts "You have #{nickels} nickels"
+    puts "You have #{pennies} pennies"
 
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -1,8 +1,14 @@
 require ('rspec')
 require('coin_combinations')
+require('pry')
 
-describe ('Fixnum#coin_combinations') do
-  it('takes an input and converts it into a Float') do
-    expect((25).coin_combinations()).to(eq(25.0))
-  end
+describe ('Float#coin_combinations') do
+#   it('takes an input and converts it into a Float') do
+#     test_example = 25
+#     expect((test_example).coin_combinations()).to(eq(25.0))
+#   end
+  #
+   it('takes input and determines how many quarters') do
+    expect((0.99).coin_combinations()).to(eq(3))
+   end
 end
